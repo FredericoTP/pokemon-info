27349b53f8f2443a877e01aae6b01d640d500414
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-26 13:54:50 -0300

    Adiciona botões para avançar e recuar na pokedex

--- pokemon-info/src/pages/Home.jsx
+++ pokemon-info/src/pages/Home.jsx
@@ -6,7 +6,12 @@ class Home extends React.Component {
     super();
     this.state = {
       pokemonData: [],
+      limit: 15,
+      set: 0,
     };
+
+    this.handleClickIncrease = this.handleClickIncrease.bind(this);
+    this.handleClickDecrease = this.handleClickDecrease.bind(this);
   }
 
   async componentDidMount() {
@@ -18,18 +23,68 @@ class Home extends React.Component {
     });
   }
 
+  async handleClickIncrease() {
+    const { limit, set } = this.state;
+    let numberOne = set + 15;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${numberOne}`;
+    console.log(url);
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      pokemonData: data.results,
+    });
+
+    this.setState((prevState) => ({
+      set: prevState.set + 15,
+    }))
+  }
+
+  async handleClickDecrease() {
+    const { limit, set } = this.state;
+    let numberOne = set - 15;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${numberOne}`;
+    console.log(url);
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      pokemonData: data.results,
+    });
+
+    this.setState((prevState) => ({
+      set: prevState.set - 15,
+    }))
+  }
+
   render() {
-    const { pokemonData } = this.state;
+    const { pokemonData, set } = this.state;
     return (
       <section>
-        {
-          pokemonData.map((item, index) => (
-            <PokemonSimplified
-              key={ `${item.name}${index}` }
-              pokemon={ item.url }
-            />
-          ))
-        }
+        <div>
+          {
+            pokemonData.map((item, index) => (
+              <PokemonSimplified
+                key={ `${item.name}${index}` }
+                pokemon={ item.url }
+              />
+            ))
+          }
+        </div>
+        <div>
+          <button
+            type='button'
+            onClick={ this.handleClickDecrease }
+            disabled={ set <= 0 }
+          >
+            Anterior
+          </button>
+          <button
+            type='button'
+            onClick={ this.handleClickIncrease }
+            disabled={ set >= 1140 }
+          >
+            Próximo
+          </button>
+        </div>
       </section>
     );
   }
