827cb64b2e65b8bcb0ac28abc907fa39cebfd64d
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-11-26 10:52:40 -0300

    Chamada da API em Home e criação do componente PokemonSimplified

---
+++ pokemon-info/src/components/PokemonSimplified.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PokemonSimplified extends React.Component {
+  render() {
+    return (
+      <p>asd</p>
+    );
+  }
+}
+
+export default PokemonSimplified;
--- pokemon-info/src/pages/Home.jsx
+++ pokemon-info/src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemonData: [],
+    };
+  }
+
+  async componentDidMount() {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=15&offset=0';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      pokemonData: data,
+    });
+  }
+
   render() {
+    const { pokemonData } = this.state;
     return (
       <p>asd</p>
     );
