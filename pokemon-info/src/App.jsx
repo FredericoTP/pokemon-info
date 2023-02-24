c98f2275a11dab51b540567ebbefeff8aeeb191b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-24 10:12:17 -0300

    Changes

--- pokemon-info/src/App.jsx
+++ pokemon-info/src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={ Home } />
-      </Switch>
-    );
-  }
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Home } />
+    </Switch>
+  );
 }
 
-export default App
+export default App;
