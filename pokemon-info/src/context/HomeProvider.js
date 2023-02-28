176433cb53a665bfac09c6c7e5841ea9af870e8e
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-02-28 18:54:20 -0300

    Feat: call Api in Home

--- pokemon-info/src/context/HomeProvider.js
+++ pokemon-info/src/context/HomeProvider.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import HomeContext from "./HomeContext";
+import useFetch from '../hooks/useFetch';
 
 function HomeProvider({ children }) {
+  const {data, error, loading, fetchAllPokemon} = useFetch();
+
   return (
-    <HomeContext.Provider value={ { asd:'asd' } }>
+    <HomeContext.Provider value={ { data, error, loading, fetchAllPokemon } }>
       {children}
     </HomeContext.Provider>
   );
--- pokemon-info/src/pages/Home.jsx
+++ pokemon-info/src/pages/Home.jsx
@@ -1,4 +1,19 @@
+import { useContext, useEffect } from "react";
+import HomeContext from "../context/HomeContext";
+
 function Home() {
+  const { data, fetchAllPokemon } = useContext(HomeContext);
+
+  useEffect(() => {
+    const callApi = async () => {
+      await fetchAllPokemon('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+    };
+
+    callApi();
+  }, [])
+
+  console.log(data);
+
   return (
     <div>
       <p>asd</p>
